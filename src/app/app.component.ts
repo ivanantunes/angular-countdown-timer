745c922e1b71eb42b1e5df69fca5c2bb1558c9f1
ivanantunes <ivanantnes75@gmail.com>
2020-05-22 16:29:53 -0300

    form event implements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,42 @@
-import { Component } from '@angular/core';
+// Native Angular
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  formEvent: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit() {
+    this.formEvent = this.formBuilder.group({
+      eventName: ['', Validators.compose([Validators.required, Validators.minLength(1)])],
+      eventDate: ['', Validators.compose([Validators.required])]
+    });
+  }
+
+  public registerEvent() {
+    const event = {
+      name: this.eventName.value,
+      date: this.eventDate.value
+    };
+    console.log(event);
+  }
+
+  public resetForm() {
+    this.formEvent.reset();
+  }
+
+  get eventName() {
+    return this.formEvent.get('eventName');
+  }
+  get eventDate() {
+    return this.formEvent.get('eventDate');
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
+// Angular Core
 import { NgModule } from '@angular/core';
 
+// Browser
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Routing
 import { AppRoutingModule } from './app-routing.module';
+
+// Forms
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+// Material
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+// Ngx
+import { NgxMatDatetimePickerModule, NgxMatTimepickerModule, NgxMatNativeDateModule } from '@angular-material-components/datetime-picker';
+
+// Component
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -9,8 +30,28 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    // Browser
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+
+    // Fomrs
+    FormsModule,
+    ReactiveFormsModule,
+
+    // Routing
+    AppRoutingModule,
+
+    // Material
+    MatCardModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatButtonModule,
+
+    // Ngx
+    NgxMatDatetimePickerModule,
+    NgxMatTimepickerModule,
+    NgxMatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
